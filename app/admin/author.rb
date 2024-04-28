dd556cb4a1cb478e6f8518b8a033b52bf5fbe10a
cuongnc0211 <cuongnguyenfu@gmail.com>
2024-04-28 23:08:51 +0700

    fix filter author

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -2,4 +2,6 @@ ActiveAdmin.register Author do
   menu parent: 'Manage Books', priority: 2, label: 'Author'
 
   permit_params :name, :biography, :birth_date
+
+  filter :name_cont, label: 'Name'
 end
