ae8944bee245e09f14068748275e5ab1e67bea35
cuongnc0211 <cuongnguyenfu@gmail.com>
2024-04-29 15:10:05 +0700

    user register

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
   def index
+    @show_header = true
     @pagy, @books = pagy(Book.includes(:author, :category, :book_versions).all)
     @categories = Category.all
   end
 
   def show
+    @show_header = true
     @book = Book.includes(:author, :category, :book_versions).find(params[:id])
   end
 end
