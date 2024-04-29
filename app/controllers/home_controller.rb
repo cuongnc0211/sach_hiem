a5d270164891c107150af6898a3d0a6abc891d01
cuongnc0211 <cuongnguyenfu@gmail.com>
2024-04-29 16:03:45 +0700

    change home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @show_header = true
     @pagy, @books = pagy(Book.includes(:author, :category, :book_versions).all)
+    @categories = Category.all
   end
 end
