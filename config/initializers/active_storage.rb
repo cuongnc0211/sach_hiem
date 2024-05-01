9490c3453b571d049d5b9c3b6dcd0eeacc406fb5
cuongnc0211 <cuongnguyenfu@gmail.com>
2024-05-01 18:30:06 +0700

    add vips fpr heroku app

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -3,3 +3,5 @@ Rails.application.config.active_storage.url_options = {
   port: 3000, # or the port your Rails server is running on
   protocol: 'https' # or 'http' depending on your setup
 }
+
+Rails.application.config.active_storage.variant_processor = :vips
